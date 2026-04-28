019d6c543f0285ff9d80309fb54b905a35767c1b
onders86 <ondersezgin+github@gmail.com>
2026-04-28 03:10:16 +0100

    align confluence changes for error message and summary

--- app/controllers/RefundingCountryController.scala
+++ app/controllers/RefundingCountryController.scala
@@ -30,6 +30,7 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.RefundingCountryView
 
 import scala.concurrent.{ExecutionContext, Future}
+import play.api.data.FormError
 
 class RefundingCountryController @Inject()(
                                            override val messagesApi: MessagesApi,
@@ -75,15 +76,29 @@ class RefundingCountryController @Inject()(
     }
 
     form.bindFromRequest().fold(
-      formWithErrors =>
-        Future.successful(BadRequest(view(formWithErrors, countries))),
-
-      value =>
-        val baseAnswers = request.userAnswers.getOrElse(models.UserAnswers(request.userId))
-        for {
-          updatedAnswers <- Future.fromTry(baseAnswers.set(RefundingCountryPage, value))
-          _ <- sessionRepository.set(updatedAnswers)
-        } yield Redirect(navigator.nextPage(RefundingCountryPage, NormalMode, updatedAnswers))
+      formWithErrors => {
+        val typed = request.body.asFormUrlEncoded.flatMap(_.get("valueTyped").flatMap(_.headOption)).getOrElse("")
+        val adjustedForm = if (typed.trim.nonEmpty) {
+          val filtered = formWithErrors.errors.filterNot(e => e.key == "value" && e.message == "refundingCountry.error.required")
+          formWithErrors.copy(errors = filtered :+ FormError("value", "refundingCountry.error.invalid"))
+        } else {
+          formWithErrors
+        }
+        Future.successful(BadRequest(view(adjustedForm, countries)))
+      },
+      value => {
+        val allowed = countries.exists { case (name, code) => code == value || name == value }
+        if (!allowed) {
+          val formWithInvalid = form.bindFromRequest().withError("value", "refundingCountry.error.invalid")
+          Future.successful(BadRequest(view(formWithInvalid, countries)))
+        } else {
+          val baseAnswers = request.userAnswers.getOrElse(models.UserAnswers(request.userId))
+          for {
+            updatedAnswers <- Future.fromTry(baseAnswers.set(RefundingCountryPage, value))
+            _ <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(RefundingCountryPage, NormalMode, updatedAnswers))
+        }
+      }
     )
   }
 }
