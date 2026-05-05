d7f6ee1bfd00623ac7f691da21367f4b47c9ada2
onders86 <ondersezgin+github@gmail.com>
2026-05-05 09:15:03 +0100

    DTR-4268 : What language do you want to use for this claim?

---
+++ app/models/RefundingLanguage.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
+
+sealed trait RefundingLanguage
+
+object RefundingLanguage extends Enumerable.Implicits {
+
+  case object English extends WithName("english") with RefundingLanguage
+  case object German extends WithName("german") with RefundingLanguage
+  case object French extends WithName("french") with RefundingLanguage
+  case object Dutch extends WithName("dutch") with RefundingLanguage
+  case object Bulgarian extends WithName("bulgarian") with RefundingLanguage
+  case object Spanish extends WithName("spanish") with RefundingLanguage
+  case object Turkish extends WithName("turkish") with RefundingLanguage
+  case object Czech extends WithName("czech") with RefundingLanguage
+  case object Danish extends WithName("danish") with RefundingLanguage
+  case object Estonian extends WithName("estonian") with RefundingLanguage
+  case object Finnish extends WithName("finnish") with RefundingLanguage
+  case object Swedish extends WithName("swedish") with RefundingLanguage
+  case object Italian extends WithName("italian") with RefundingLanguage
+  case object Latvian extends WithName("latvian") with RefundingLanguage
+  case object Lithuanian extends WithName("lithuanian") with RefundingLanguage
+  case object Maltese extends WithName("maltese") with RefundingLanguage
+  case object Polish extends WithName("polish") with RefundingLanguage
+  case object Portuguese extends WithName("portuguese") with RefundingLanguage
+  case object Romanian extends WithName("romanian") with RefundingLanguage
+  case object Hungarian extends WithName("hungarian") with RefundingLanguage
+  case object Greek extends WithName("greek") with RefundingLanguage
+  case object Slovak extends WithName("slovak") with RefundingLanguage
+  case object Slovenian extends WithName("slovenian") with RefundingLanguage
+  case object Irish extends WithName("irish") with RefundingLanguage
+
+  val values: Seq[RefundingLanguage] = Seq(
+    English, German, French, Dutch, Bulgarian, Spanish, Turkish, Czech,
+    Danish, Estonian, Finnish, Swedish, Italian, Latvian, Lithuanian,
+    Maltese, Polish, Portuguese, Romanian, Hungarian, Greek, Slovak, Slovenian, Irish
+  )
+
+  def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map {
+    case (value, index) =>
+      RadioItem(
+        content = Text(messages(s"refundingLanguage.${value.toString}")),
+        value   = Some(value.toString),
+        id      = Some(s"value_$index")
+      )
+  }
+
+  implicit val enumerable: Enumerable[RefundingLanguage] =
+    Enumerable(values.map(v => v.toString -> v): _*)
+}
---
+++ test/controllers/RefundingLanguageControllerSpec.scala
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import play.api.inject.bind
+import play.api.Configuration
+import com.typesafe.config.ConfigFactory
+import repositories.SessionRepository
+import pages.RefundingCountryPage
+import pages.RefundingLanguagePage
+import play.api.mvc.Call
+
+class RefundingLanguageControllerSpec extends SpecBase with MockitoSugar {
+
+  val onwardRoute = Call("GET", "/foo")
+
+  "RefundingLanguage Controller" - {
+
+    "must return OK and the correct view for a GET when country present" in {
+
+      val userAnswers = emptyUserAnswers.set(RefundingCountryPage, "AT").success.value
+
+      val cfg = Configuration(ConfigFactory.parseString("language.mapping.AT=[\"german\", \"english\"]"))
+      val mappingSvc = new _root_.services.ConfigLanguageMapping(cfg)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(
+          bind[_root_.services.ConfigLanguageMapping].toInstance(mappingSvc)
+        ).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.RefundingLanguageController.onPageLoad(models.NormalMode).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[views.html.RefundingLanguageView]
+        val formProvider = application.injector.instanceOf[forms.RefundingLanguageFormProvider]
+        val form = formProvider()
+        val langs = mappingSvc.languagesFor("AT")
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, langs, Some(routes.RefundingCountryController.onPageLoad().url), models.NormalMode)(request, messages(application)).toString
+
+        // also supports CheckMode change route
+        val changeRequest = FakeRequest(GET, routes.RefundingLanguageController.onPageLoad(models.CheckMode).url)
+        val changeResult = route(application, changeRequest).value
+        status(changeResult) mustEqual OK
+        contentAsString(changeResult) mustEqual view(form, langs, Some(routes.RefundingCountryController.onPageLoad().url), models.CheckMode)(changeRequest, messages(application)).toString
+      }
+    }
+
+    "must redirect to JourneyRecovery when no country present" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.RefundingLanguageController.onPageLoad(models.NormalMode).url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to next page when valid data submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+      when(mockSessionRepository.set(any())) thenReturn scala.concurrent.Future.successful(true)
+
+      val userAnswers = emptyUserAnswers.set(RefundingCountryPage, "AT").success.value
+
+      val cfg = Configuration(ConfigFactory.parseString("language.mapping.AT=[\"german\", \"english\"]"))
+      val mappingSvc = new _root_.services.ConfigLanguageMapping(cfg)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(
+          bind[navigation.Navigator].toInstance(new navigation.FakeNavigator(onwardRoute)),
+          bind[repositories.SessionRepository].toInstance(mockSessionRepository),
+          bind[_root_.services.ConfigLanguageMapping].toInstance(mappingSvc)
+        ).build()
+
+      running(application) {
+        val request = FakeRequest(POST, routes.RefundingLanguageController.onSubmit(models.NormalMode).url)
+          .withFormUrlEncodedBody(("value", "english"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+
+        verify(mockSessionRepository).set(any())
+
+        // also supports CheckMode submission
+        val checkRequest = FakeRequest(POST, routes.RefundingLanguageController.onSubmit(models.CheckMode).url)
+          .withFormUrlEncodedBody(("value", "english"))
+
+        val checkResult = route(application, checkRequest).value
+        status(checkResult) mustEqual SEE_OTHER
+        redirectLocation(checkResult).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return Bad Request when invalid data submitted" in {
+
+      val userAnswers = emptyUserAnswers.set(RefundingCountryPage, "AT").success.value
+
+      val cfg = Configuration(ConfigFactory.parseString("language.mapping.AT=[\"german\", \"english\"]"))
+      val mappingSvc = new _root_.services.ConfigLanguageMapping(cfg)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(bind[_root_.services.ConfigLanguageMapping].toInstance(mappingSvc)).build()
+
+      running(application) {
+        val request = FakeRequest(POST, routes.RefundingLanguageController.onSubmit(models.NormalMode).url)
+          .withFormUrlEncodedBody(("value", ""))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) must include(messages(application)("refundingLanguage.error.required"))
+      }
+    }
+  }
+}
