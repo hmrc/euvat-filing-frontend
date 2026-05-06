3158f40de8137423cd188466f85a8a8cc287f82e
vshiligin <103203771+vshiligin@users.noreply.github.com>
2026-05-06 09:58:49 +0100

    DTR-4274 add RA5.0 Purchase type page
    5 radio options per the Confluence spec (Fuel, Transport, FoodAndDrink, Luxuries, Other). Controller follows the existing requireData pattern; Navigator routes to JourneyRecoveryController as a placeholder until RA6.0 / RA8.0 are implemented.

---
+++ app/models/PurchaseType.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
+
+sealed trait PurchaseType
+
+object PurchaseType extends Enumerable.Implicits {
+
+  case object Fuel         extends WithName("fuel")         with PurchaseType
+  case object Transport    extends WithName("transport")    with PurchaseType
+  case object FoodAndDrink extends WithName("foodAndDrink") with PurchaseType
+  case object Luxuries     extends WithName("luxuries")     with PurchaseType
+  case object Other        extends WithName("other")        with PurchaseType
+
+  val values: Seq[PurchaseType] = Seq(Fuel, Transport, FoodAndDrink, Luxuries, Other)
+
+  def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map { case (value, index) =>
+    RadioItem(
+      content = Text(messages(s"purchaseType.${value.toString}")),
+      value   = Some(value.toString),
+      id      = Some(s"value_$index")
+    )
+  }
+
+  implicit val enumerable: Enumerable[PurchaseType] =
+    Enumerable(values.map(v => v.toString -> v)*)
+}
---
+++ test/controllers/PurchaseTypeControllerSpec.scala
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import forms.PurchaseTypeFormProvider
+import models.{NormalMode, PurchaseType}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.PurchaseTypePage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.PurchaseTypeView
+
+import scala.concurrent.Future
+
+class PurchaseTypeControllerSpec extends SpecBase with MockitoSugar {
+
+  val onwardRoute = Call("GET", "/foo")
+
+  lazy val purchaseTypeRoute       = routes.PurchaseTypeController.onPageLoad(NormalMode).url
+  lazy val purchaseTypeSubmitRoute = routes.PurchaseTypeController.onSubmit(NormalMode).url
+
+  "PurchaseType Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, purchaseTypeRoute)
+        val result  = route(application, request).value
+
+        val view         = application.injector.instanceOf[PurchaseTypeView]
+        val formProvider = application.injector.instanceOf[PurchaseTypeFormProvider]
+        val form         = formProvider()
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, None)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery when no existing data is found on GET" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, purchaseTypeRoute)
+        val result  = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must populate the view with the saved value on GET" in {
+
+      val userAnswers = emptyUserAnswers.set(PurchaseTypePage, PurchaseType.Fuel).success.value
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, purchaseTypeRoute)
+        val result  = route(application, request).value
+
+        val view         = application.injector.instanceOf[PurchaseTypeView]
+        val formProvider = application.injector.instanceOf[PurchaseTypeFormProvider]
+        val form         = formProvider().fill(PurchaseType.Fuel)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, None)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page and persist the answer when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+      when(mockSessionRepository.set(any())).thenReturn(Future.successful(true))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(
+          bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+          bind[SessionRepository].toInstance(mockSessionRepository)
+        )
+        .build()
+
+      running(application) {
+        val request = FakeRequest(POST, purchaseTypeSubmitRoute)
+          .withFormUrlEncodedBody("value" -> PurchaseType.Fuel.toString)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+        verify(mockSessionRepository, times(1)).set(any())
+      }
+    }
+
+    "must return Bad Request and errors when no value is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(POST, purchaseTypeSubmitRoute)
+          .withFormUrlEncodedBody("value" -> "")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) must include(messages(application)("purchaseType.error.required"))
+      }
+    }
+
+    "must return Bad Request and errors when an invalid value is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(POST, purchaseTypeSubmitRoute)
+          .withFormUrlEncodedBody("value" -> "notARealOption")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) must include(messages(application)("purchaseType.error.required"))
+      }
+    }
+
+    "must redirect to Journey Recovery when no existing data is found on POST" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(POST, purchaseTypeSubmitRoute)
+          .withFormUrlEncodedBody("value" -> PurchaseType.Fuel.toString)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
