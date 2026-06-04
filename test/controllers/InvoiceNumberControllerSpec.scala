b9693790312991d7e8c01aa80ab2a03805c65286
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-06-04 16:19:09 +0100

    DTR-4285 fix formatting

--- test/controllers/InvoiceNumberControllerSpec.scala
+++ test/controllers/InvoiceNumberControllerSpec.scala
@@ -126,9 +126,7 @@ class InvoiceNumberControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode, backLink(NormalMode))(request,
-                             messages(application)
-                            ).toString
+        contentAsString(result) mustEqual view(boundForm, NormalMode, backLink(NormalMode))(request, messages(application)).toString
       }
     }
 
