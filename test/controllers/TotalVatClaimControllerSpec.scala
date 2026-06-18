9adbfe6f8d26c8a786a5dc768be66c5c98d4592c
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-06-18 18:21:24 +0100

    DTR-4294 add tests

--- test/controllers/TotalVatClaimControllerSpec.scala
+++ test/controllers/TotalVatClaimControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
@@ -11,7 +27,7 @@ import pages.TotalVatClaimPage
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.Helpers.*
 import repositories.SessionRepository
 import views.html.TotalVatClaimView
 
@@ -26,7 +42,9 @@ class TotalVatClaimControllerSpec extends SpecBase with MockitoSugar {
 
   val validAnswer = 0
 
-  lazy val totalVatClaimRoute = routes.TotalVatClaimController.onPageLoad(NormalMode).url
+  lazy val totalVatClaimRoute = routes.TotalVatClaimController.onPageLoad().url
+
+  def backLink: Call = routes.JourneyRecoveryController.onPageLoad()
 
   "TotalVatClaim Controller" - {
 
@@ -42,7 +60,7 @@ class TotalVatClaimControllerSpec extends SpecBase with MockitoSugar {
         val view = application.injector.instanceOf[TotalVatClaimView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, NormalMode, backLink)(request, messages(application)).toString
       }
     }
 
@@ -60,7 +78,7 @@ class TotalVatClaimControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode, backLink)(request, messages(application)).toString
       }
     }
 
@@ -106,7 +124,21 @@ class TotalVatClaimControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, NormalMode, backLink)(request, messages(application)).toString
+      }
+    }
+
+    "must have the correct back link" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, totalVatClaimRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) must include(routes.JourneyRecoveryController.onPageLoad().url)
       }
     }
 
