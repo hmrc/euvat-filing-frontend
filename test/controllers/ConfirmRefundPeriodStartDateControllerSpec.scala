3784dcd8f14b5ba158cd9ea33cc464d05af1e984
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-08-10 20:44:22 +0100

    DTR-7280 refactor code, fix bugs, add tests

--- test/controllers/ConfirmRefundPeriodStartDateControllerSpec.scala
+++ test/controllers/ConfirmRefundPeriodStartDateControllerSpec.scala
@@ -1,9 +1,13 @@
 package controllers
 
 import base.SpecBase
+import models.{CheckMode, NormalMode, RefundPeriod}
+import pages.RefundPeriodPage
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.Helpers.*
 import views.html.ConfirmRefundPeriodStartDateView
+
+import java.time.{LocalDate, LocalDateTime, YearMonth}
 
 class ConfirmRefundPeriodStartDateControllerSpec extends SpecBase {
 
@@ -11,18 +15,104 @@ class ConfirmRefundPeriodStartDateControllerSpec extends SpecBase {
 
     "must return OK and the correct view for a GET" in {
 
+      val refundPeriod = RefundPeriod(
+        startDate = LocalDateTime.of(2024, 10, 1, 0, 0),
+        endDate   = LocalDateTime.of(2024, 12, 31, 23, 59)
+      )
+
+      val userAnswers = emptyUserAnswers.set(RefundPeriodPage, refundPeriod).success.value
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ConfirmRefundPeriodStartDateController.onPageLoad(NormalMode).url)
+        val result = route(application, request).value
+        val view = application.injector.instanceOf[ConfirmRefundPeriodStartDateView]
+
+        val expectedStartDate = "10/2024"
+        val expectedMinDate = "01/2025" // correct as of today's real date
+        val expectedCall = routes.RefundPeriodController.onPageLoad(NormalMode)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(expectedStartDate, expectedMinDate, expectedCall, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must return OK and the correct view for a GET in CheckMode" in {
+
+      val refundPeriod = RefundPeriod(
+        startDate = LocalDateTime.of(2024, 10, 1, 0, 0),
+        endDate   = LocalDateTime.of(2024, 12, 31, 23, 59)
+      )
+
+      val userAnswers = emptyUserAnswers.set(RefundPeriodPage, refundPeriod).success.value
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ConfirmRefundPeriodStartDateController.onPageLoad(CheckMode).url)
+        val result = route(application, request).value
+        val view = application.injector.instanceOf[ConfirmRefundPeriodStartDateView]
+
+        val expectedStartDate = "10/2024"
+        val expectedMinDate = "01/2025"
+        val expectedCall = routes.RefundPeriodController.onPageLoad(CheckMode)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(expectedStartDate, expectedMinDate, expectedCall, CheckMode)(request, messages(application)).toString
+      }
+    }
+
+    " must redirect to JourneyRecoveryController for a GET in NormalMode if no refund period data exists" in {
+
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
       running(application) {
-        val request = FakeRequest(GET, routes.ConfirmRefundPeriodStartDateController.onPageLoad().url)
-
+        val request = FakeRequest(GET, routes.ConfirmRefundPeriodStartDateController.onPageLoad(NormalMode).url)
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[ConfirmRefundPeriodStartDateView]
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
+
+    " must redirect to JourneyRecoveryController for a GET in CheckMode if no refund period data exists" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ConfirmRefundPeriodStartDateController.onPageLoad(CheckMode).url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    " must redirect to ContactDetailsController on Submit in NormalMode" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(POST, routes.ConfirmRefundPeriodStartDateController.onSubmit(NormalMode).url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.ContactDetailsController.onPageLoad(NormalMode).url
+      }
+    }
+
+    " must redirect to CheckYourClaimDetailsController on Submit in CheckMode" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(POST, routes.ConfirmRefundPeriodStartDateController.onSubmit(CheckMode).url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.CheckYourClaimDetailsController.onPageLoad().url
+      }
+    }
+
+    
   }
 }
