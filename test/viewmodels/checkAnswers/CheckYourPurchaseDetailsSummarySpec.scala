5ad49abcf56ddaf35b52758026ab5727112f6056
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2026-09-08 12:45:11 +0100

    DTR-8173 resolve error after rebase

--- test/viewmodels/checkAnswers/CheckYourPurchaseDetailsSummarySpec.scala
+++ test/viewmodels/checkAnswers/CheckYourPurchaseDetailsSummarySpec.scala
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package viewmodels.checkAnswers
 
 import base.SpecBase
 import controllers.routes
-import models.{CheckMode, FoodAndDrink, Fuel, InvoiceType, Other, PurchaseType, SupplierTaxNumber, UserAnswers}
+import models.{CheckMode, FoodAndDrink, Fuel, InvoiceType, Other, PurchaseAndImportType, SupplierTaxNumber, UserAnswers}
 import pages.*
 import play.api.i18n.Messages
 import play.api.test.Helpers.*
@@ -201,7 +216,7 @@ class CheckYourPurchaseDetailsSummarySpec extends SpecBase {
           rows.exists(_._1 == msgs("checkYourPurchaseDetails.refundingCurrency.label")) mustBe true
 
           val currencyRow = rows.find(_._1 == msgs("checkYourPurchaseDetails.refundingCurrency.label")).value
-          currencyRow._3.head._1 mustEqual routes.RefundingCurrencyController.onPageLoad(CheckMode).url
+          currencyRow._3.head._1 mustEqual controllers.purchase.routes.RefundingCurrencyController.onPageLoad(CheckMode).url
         }
       }
     }
